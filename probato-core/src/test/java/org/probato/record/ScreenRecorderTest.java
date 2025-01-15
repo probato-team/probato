4af89f123a86b065316baf632a9e25cfb651689a
felipe.rudolfe <felipe.rudolfe>
2025-01-15 14:17:40 -0300

    Migrate record to probato

---
+++ probato-core/src/test/java/org/probato/record/ScreenRecorderTest.java
@@ -0,0 +1,43 @@
+package org.probato.record;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.awt.AWTException;
+import java.io.File;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.probato.model.Dimension;
+import org.probato.model.Video;
+import org.probato.model.type.DimensionMode;
+import org.probato.model.type.Quality;
+import org.probato.model.type.Screen;
+
+@DisplayName("Test -> ScreenRecorder")
+class ScreenRecorderTest {
+
+	@Test
+	@DisplayName("Should record successfully")
+	void shouldRecordSuccessfully() throws AWTException {
+
+		var video = new Video();
+		video.setFrameRate(100.0);
+		video.setQuality(Quality.HIGH);
+
+		var dimension = new Dimension();
+		dimension.setHeight(1);
+		dimension.setWidth(1);
+		dimension.setMode(DimensionMode.CUSTOM);
+		
+		var directory = new File("C:/probato/temp");
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+
+		var screen = new ScreenRecorder("C:/probato/temp/test-screen-record.mp4", Screen.PRINCIPAL, video, dimension);
+		screen.startCapture();
+		screen.stopCapture();
+
+		assertTrue(Boolean.TRUE);
+	}
+}
